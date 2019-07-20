135b1b5a6869589f5311876cc42d940f32bad9fc
lyine <liuzhengyuan@hotmail.com>
2019-07-20 13:44:34 +0800

    add more key

--- image-capture.py
+++ image-capture.py
@@ -8,10 +8,9 @@ import time
 from keycontrol import KeyControl
 
 
-
-
 import os
 import threading
+
 
 def connect(retry):
     while True:
@@ -28,35 +27,36 @@ def connect(retry):
             sys.exit(1)
         time.sleep(1)
 
+
 def key_loop():
     global STATE
-    key=KeyControl()
+    key = KeyControl()
     key.start()
     while True:
-        state=key.get_key()
+        state = key.get_key()
         if state is not None:
-            STATE=state
+            STATE = state
 
-            
-def cap_loop(ver,rb,sleep_time):
-    global lock,STATE
-    key=KeyControl()
-    cap=ImgCap(ver)
+
+def cap_loop(ver, rb, sleep_time):
+    global lock, STATE
+    key = KeyControl()
+    cap = ImgCap(ver)
     key.start()
     while True:
         # lock.acquire()
-        state=key.get_key()
+        state = key.get_key()
         if state is not None:
-            STATE=state
+            STATE = state
         try:
-            cap.capture(rb.get_image(),STATE)
+            cap.capture(rb.get_image(), STATE)
         except Exception as e:
             time.sleep(sleep_time)
             continue
         time.sleep(sleep_time)
 
 
-def sample_data(ver,speed=0.05):
+def sample_data(ver, speed=0.05):
     client_id = connect(10)
     rb = Hexapod(client_id)
     vrep.simxStartSimulation(client_id, vrep.simx_opmode_blocking)
@@ -64,61 +64,71 @@ def sample_data(ver,speed=0.05):
     rb.step_init()
     position_state = 0
 
-    #creat thread
-    global STATE,lock
-    STATE=0
+    # creat thread
+    global STATE, lock
+    STATE = 0
     # lock=threading.Lock()
-    cap_th=threading.Thread(target=cap_loop,args=(ver,rb,0.05),name='cap_th')
+    cap_th = threading.Thread(
+        target=cap_loop, args=(ver, rb, 0.05), name='cap_th')
     # key_th=threading.Thread(target=key_loop,name='key_th')
 
     cap_th.start()
     # key_th.start()
 
     while True:
-        if STATE==0:
+        if STATE == 0:
             rb.one_step(0.003)
-        if STATE==1:
+        #right
+        if STATE == 1:
+            rb.turn_left([5, 45])
+        elif STATE == 2:
+            rb.turn_left([20, 36])
+        elif STATE == 3:
             rb.turn_left([20, 30])
-        if STATE==2:
-            rb.turn_right([20,36])
+        #left
+        elif STATE == 4:
+            rb.turn_right([5, 45])
+        elif STATE == 5:
+            rb.turn_right([20, 36])
+        elif STATE == 6:
+            rb.turn_right([20, 30])
 
 
 class ImgCap(object):
-    def __init__(self,ver):
-        self.index=0
-        self.ver=ver
+    def __init__(self, ver):
+        self.index = 0
+        self.ver = ver
         self.create_dir('./image/{}'.format(self.ver))
-    
-    def capture(self,data, label):
-        data=cv2.cvtColor(data,cv2.COLOR_RGB2GRAY)
-        cv2.imwrite('./image/{0}/image_{1}_{2}.jpg'.format(self.ver,self.index, label), data)
-        self.index+=1
 
-    def create_dir(self,dirs):
+    def capture(self, data, label):
+        data = cv2.cvtColor(data, cv2.COLOR_RGB2GRAY)
+        cv2.imwrite(
+            './image/{0}/image_{1}_{2}.jpg'.format(self.ver, self.index, label), data)
+        self.index += 1
+
+    def create_dir(self, dirs):
         if not os.path.exists(dirs):
             os.makedirs(dirs)
         else:
-            file_list=os.listdir('./image/{}'.format(self.ver))
-            if not file_list==[]:
+            file_list = os.listdir('./image/{}'.format(self.ver))
+            if not file_list == []:
                 for name in file_list:
-                    os.remove('./image/{0}/{1}'.format(self.ver,name))
-
+                    os.remove('./image/{0}/{1}'.format(self.ver, name))
 
 
 def show_img(img):
-    cv2.namedWindow("Image") 
-    cv2.imshow("Image", img) 
-    cv2.waitKey (0)
+    cv2.namedWindow("Image")
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
 def cv2_test():
-    img=cv2.imread('./image/0/image_4_0.jpg')
+    img = cv2.imread('./image/0/image_4_0.jpg')
 
-
-        
 
 if __name__ == "__main__":
-    sample_data(5,0.01)
+    sample_data(5, 0.01)
     # test_robot()
     # print(predict(model,'./image/0/image_16_1.jpg'))
     pass
--- keycontrol.py
+++ keycontrol.py
@@ -10,24 +10,33 @@ class KeyControl(object):
     def start(self):
         screen = pygame.display.set_mode(self.size)  # 显示窗口
         direction = []
-    
+
     def get_key(self):
-        state=None   
+        state = None
         for event in pygame.event.get():  # 遍历所有事件
-            if event.type == pygame.KEYDOWN and event.key ==pygame.K_RIGHT:
-                state=2
-            elif event.type == pygame.KEYDOWN and event.key ==pygame.K_LEFT:
-                state=1
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_f:
+                state = 1
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_d:
+                state = 2
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+                state = 3
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_j:
+                state = 4
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_k:
+                state = 5
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_l:
+                state = 6
             else:
-                state=0
+                state = 0
             if event.type == pygame.QUIT:  # 如果单击关闭窗口，则退出
                 self.end()
         return state
+
     def end(self):
         pygame.quit()  # 退出pygame
         sys.exit()
 
+
 if __name__ == "__main__":
-    key=KeyControl()
+    key = KeyControl()
     key.start()
-    
