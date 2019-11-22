7396ac322c8360ea5f573c6157a7a4df564dd8ad
lyineee <liuzhengyuan@hotmail.com>
2019-11-22 20:32:18 +0800

    Delete test_by.py

--- test_by.py
+++
@@ -1,6 +0,0 @@
-from matplotlib import pyplot as plt
-import numpy as np
-
-
-def black_box(mi_0_s, mi_0_e, mi_1_s, mi_1_m, mi_1_e, mi_2_s, mi_2_m, mi_2_e, ah_0_s, ah_0_e,ah_1_s, ah_1_e,ah_2_s, ah_2_e,ba_0_s, ba_0_e,ba_1_s, ba_1_e,ba_2_s, ba_2_e, delay):
-    pass
